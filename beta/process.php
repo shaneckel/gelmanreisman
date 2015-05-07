423278545224289596b620748d5282e8e215bf78
Shane Eckel <shaneckel@gmail.com>
2015-05-07 13:45:14 -0400

    more testing

--- beta/process.php
+++ beta/process.php
@@ -10,25 +10,25 @@ if( isset($_POST) ){
 
   $dataInput = json_decode(file_get_contents('php://input'));
   
-  echo($dataInput);
+  $formname = $dataInput->name;
+  $formcomment = nl2br($dataInput->comment);
+  $formemail = $dataInput->email;
+  $formphone = $dataInput->phone;
+  $formlaw = $dataInput->law;
+  
+  $data['stuff'] = $dataInput;
 
-  $name = $dataInput->name;
-  $comment = nl2br($dataInput->comment);
-  $email = $dataInput->email;
-  $phone = $dataInput->phone;
-  $law = $dataInput->law;
-
-  if (empty( $dataInput->name) ){
+  if (empty( $dataInput->formname) ){
     $errors['name'] = 'name is required.';
     $formok = false;
   }
  
-  if (empty( $dataInput->phone) ){
+  if (empty( $dataInput->formphone) ){
     $errors['phone'] = 'phone is required.';
     $formok = false;
   }
 
-  if (empty( $dataInput->email) ){
+  if (empty( $dataInput->formemail) ){
     $errors['email'] = 'email is required.';
     $formok = false;
   }
@@ -38,7 +38,7 @@ if( isset($_POST) ){
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
     $headers .= 'MIME-Version: 1.0' . "\r\n";
  
-    $emailbody = "<h1 style='font-size:15px;color:#5b3f44;padding:20px 20px 10px 20px;font-family:sans-serif'>Gelman and Reisman Website Email</h1><h1 style=font-size:16px;padding:20px;background:#5b3f44;color:#fff;font-family:sans-serif;font-weight:300>from: <span style=font-weight:900>{$name}</span></h1><h2 style='font-weight:300;font-family:sans-serif;text-transform:uppercase;font-size:12px;padding:10px 20px 5px 20px'>return email</h2><p style='font-size:16px;padding:5px 20px 22px 20px;line-height:22px'>{$email}</p><p><h2 style='font-weight:300;font-family:sans-serif;text-transform:uppercase;font-size:12px;padding:10px 20px 5px 20px'>return phone number</h2></p><p style='font-size:16px;padding:5px 20px 22px 20px;line-height:22px'>{$phone}</p><p><h2 style='font-weight:300;font-family:sans-serif;text-transform:uppercase;font-size:12px;padding:10px 20px 5px 20px'>additional comment</h2></p><p style='font-size:16px;padding:5px 20px 22px 20px;line-height:22px'>{$comment}</p><h2 style='font-weight:300;font-family:sans-serif;text-transform:uppercase;font-size:12px;padding:10px 20px 5px 20px'>interested law practice</h2><p style='font-size:16px;padding:5px 20px 22px 20px;line-height:22px'>{$law}</p>";
+    $emailbody = "<h1 style='font-size:15px;color:#5b3f44;padding:20px 20px 10px 20px;font-family:sans-serif'>Gelman and Reisman Website Email</h1><h1 style=font-size:16px;padding:20px;background:#5b3f44;color:#fff;font-family:sans-serif;font-weight:300>from: <span style=font-weight:900>{$formname}</span></h1><h2 style='font-weight:300;font-family:sans-serif;text-transform:uppercase;font-size:12px;padding:10px 20px 5px 20px'>return email</h2><p style='font-size:16px;padding:5px 20px 22px 20px;line-height:22px'>{$formemail}</p><p><h2 style='font-weight:300;font-family:sans-serif;text-transform:uppercase;font-size:12px;padding:10px 20px 5px 20px'>return phone number</h2></p><p style='font-size:16px;padding:5px 20px 22px 20px;line-height:22px'>{$formphone}</p><p><h2 style='font-weight:300;font-family:sans-serif;text-transform:uppercase;font-size:12px;padding:10px 20px 5px 20px'>additional comment</h2></p><p style='font-size:16px;padding:5px 20px 22px 20px;line-height:22px'>{$formcomment}</p><h2 style='font-weight:300;font-family:sans-serif;text-transform:uppercase;font-size:12px;padding:10px 20px 5px 20px'>interested law practice</h2><p style='font-size:16px;padding:5px 20px 22px 20px;line-height:22px'>{$formlaw}</p>";
     $emailMessage = "GelmanAndReisman.com Email";
 
     mail("shaneckel@gmail.com",$emailMessage,$emailbody,$headers);
